0820b9d69729100190b9a489f438ff4f0eb2faaa
admin <admin@DESKTOP-GH37NK7>
2024-01-16 17:35:25 +0900

    116실습.

--- hgjava/src/com/yedam/todo/todo116.java
+++ hgjava/src/com/yedam/todo/todo116.java
@@ -1,5 +1,15 @@
 package com.yedam.todo;
 
-public class todo116 {
+import java.util.Scanner;
 
+public class todo116 {
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		System.out.println("값을 입력>>");
+		String input = scn.nextLine();			
+		while(input.equals("quit")) { //둘 값이 같으면
+		// 입력값은 홍길동 입니다.
+		// "quit" 값이 들어오면 <<end of prog>>
+		}
+	}
 }
